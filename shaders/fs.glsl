5b9aeb9e549798b7a763f19cbc763ce6494bccb2
Mehdi <mehdi.mehdikhani@mail.polimi.it>
2020-07-25 23:50:37 +0200

    ambient light color added

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -9,6 +9,7 @@ out vec4 outColor;
 uniform vec3 mDiffColor;
 uniform vec3 lightDirection; 
 uniform vec3 lightColor; 
+uniform vec3 ambientLightcolor;
 
 uniform sampler2D u_texture;
 
@@ -16,5 +17,5 @@ void main() {
     vec3 nNormal = normalize(fsNormal);
     vec3 lDir = (lightDirection); 
     vec3 lambertColor = mDiffColor * lightColor * dot(-lDir,nNormal);
-    outColor = texture(u_texture,uvFS) * vec4(clamp(lambertColor, 0.0, 1.0), 1.0);
+    outColor = texture(u_texture,uvFS) * vec4(clamp(lambertColor + ambientLightcolor, 0.0, 1.0), 1.0);
 }
